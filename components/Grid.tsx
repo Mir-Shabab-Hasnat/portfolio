5b640221862aadaf224f20af6fbae4ab80d7489f
unknown <shababhasnat@gmail.com>
2024-09-05 18:51:58 -0600

    changes made to bento grid items

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 import { gridItems } from "@/data";
 
@@ -7,12 +6,18 @@ const Grid = () => {
     <section id="about">
       <BentoGrid>
         {gridItems.map
-        ((item) => (
+        (({id, title, description, className, img, imgClassName, titleClassName, spareImg}) => (
           <BentoGridItem 
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            description={item.description}
+            id={id}
+            key={id}
+            title={title}
+            description={description}
+            className={className}
+            img={img}
+            imgClassName={imgClassName}
+            titleClassName={titleClassName}
+            spareImg={spareImg}
+            
           />
         ))}
       </BentoGrid>
