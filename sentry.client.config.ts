20e0b68ef8de771fbe52a550ed7971bfb9229ce0
unknown <shababhasnat@gmail.com>
2024-09-07 23:17:12 -0600

    experience section added

---
+++ sentry.client.config.ts
@@ -0,0 +1,12 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ab8d2ca4e0090deaf07fc8b1cff0e35f@o4507873288388608.ingest.us.sentry.io/4507915438522368",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
