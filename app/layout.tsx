34e14549b6428988e8ad94964d25743f5ec36fe3
unknown <shababhasnat@gmail.com>
2024-09-12 19:49:43 -0600

    metadata changes on page description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Shabab's Portfolio",
-  description: "Modern and Minimalist JS Mastery Portfolio",
+  description: "My personal portfolio website",
 };
 
 export default function RootLayout({
